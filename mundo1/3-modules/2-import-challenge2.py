6ede8d85d013973a6cf59305e43e83392046ca4b
Andre Pedrotti <andrehpedrotti@gmail.com>
2021-11-15 13:43:45 -0300

    Update name

--- mundo1/3-modules/2-import-challenge2.py
+++ mundo1/3-modules/2-import-challenge2.py
@@ -2,10 +2,10 @@
 
 #If an import is not found execute: pip install <module_name>
 
-# Ler um número real e mostre sua porção inteira. Ex: 6.127, mostra 6
-# Ler comprimento do cateto oposto e do adjacente e calcule a hipotenusa
-# Ler um ângulo e mostre o valor do seu seno, cosseno e tangente
-# Ler quatro nomes e escolher 1 (para limpar o quadro)
-# Ler quatro nomes e ordenar randomicamente
-# Ler e reproduzir um arquivo mp3
+# 1 Ler um número real e mostre sua porção inteira. Ex: 6.127, mostra 6
+# 2 Ler comprimento do cateto oposto e do adjacente e calcule a hipotenusa
+# 3 Ler um ângulo e mostre o valor do seu seno, cosseno e tangente
+# 4 Ler quatro nomes e escolher 1 (para limpar o quadro)
+# 5 Ler quatro nomes e ordenar randomicamente
+# 6 Ler e reproduzir um arquivo mp3
 
