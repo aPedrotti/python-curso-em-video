854cacf789a192ea961bc5f9dcdd4ad98be098e8
Andre Pedrotti <andrehpedrotti@gmail.com>
2022-01-03 10:21:29 -0300

    Resolution

--- mundo1/5-conditions/challenges/1.py
+++ mundo1/5-conditions/challenges/1.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python
-
+from random import randrange
+from time import sleep
 print ("""
 Reads a number tries to discovery what the computer has (btw 0-5) and congratulate if find 
 """)
+computer_number = randrange(6)
 
+print('-=-'*20)
+print('I am going to thin a number between 0 and 5. Try to guess...')
+print('-=-'*20)
+
+num = int(input('What number did I think? ' ))
+print('Comparing...')
+sleep(3)
+
+if num == computer_number:
+    print('Congratulations you found my number ! ')
+else:
+    print('Your guess was wrong')
+    print('I though on {}'.format(computer_number))
 
