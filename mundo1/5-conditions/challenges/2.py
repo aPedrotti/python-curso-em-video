683402b26a45ad43fb5ec5531f01590646b645b0
Andre Pedrotti <andrehpedrotti@gmail.com>
2022-01-03 13:43:00 -0300

    Add resolutions

--- mundo1/5-conditions/challenges/2.py
+++ mundo1/5-conditions/challenges/2.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
+colours = {'clean':'\033[m',
+        'white':'\033[7m',
+        'blue':'\033[44m',
+        'green':'\033[30;42m',
+        'warning':'\033[0;30;43m',
+        'alert':'\033[0;30;41m'}
 
+speed_limit=80
+speed_charge=7
 print ("""
-Read a car speed and apply $7 / km above limit (80km/h)
-""")
+Read a car speed and apply ${} / km above limit ({}km/h)
+""".format(speed_charge,speed_limit))
+
+speed = int(input('Enter speed of your vehicle: '))
+
+if speed > speed_limit:
+    fee = (speed-speed_limit)*speed_charge
+    print('{}You are over the limit{}. You has to pay {}${}{} in charges'.format(colours['alert'],colours['clean'],colours['green'],fee,colours['clean']))
+    print('{}Please respect Speed Limits. It is for your safety{}'.format(colours['warning'],colours['clean']))
+else:
+    print('{}* Thanks for respecting Speed Limits (= *{}'.format(colours['green'],colours['clean']))
+print('{}Have a nice have. Drive safe{}'.format(colours['blue'],colours['clean']))
+
--- mundo1/5-conditions/challenges/3.py
+++ mundo1/5-conditions/challenges/3.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python
+colours = {'clean':'\033[m',
+        'white':'\033[7m',
+        'blue':'\033[44m',
+        'green':'\033[30;42m',
+        'warning':'\033[0;30;43m',
+        'alert':'\033[0;30;41m'}
 
 print ("""
-Check if the number is Even or Odd
+Check if the number is Even (rest when spliting = 0 )or Odd (rest when spliting = 1)
 """)
+
+number = int(input('Enter a number: '))
+
+if (number % 2 ) == 0:
+    print('Your number {} is {}EVEN{}'.format(number,colours['blue'],colours['clean']))
+else:
+    print('Your number {} is {}ODD{}'.format(number,colours['green'],colours['clean']))
