8941069e6a6264c91d991ec467aad275ecd6495e
Andre Pedrotti <andrehpedrotti@gmail.com>
2022-01-08 14:21:25 -0300

    Add counter

--- mundo2/4-loops-with-breaks/challenges/4.py
+++ mundo2/4-loops-with-breaks/challenges/4.py
@@ -4,7 +4,7 @@ Read age and sex and ask if want to continue:
 Then shows how much people are older than 18, how much men, and how much women younger than 20
 """)
 
-count=males=females=0
+maiority=count=males=females=0
 while True:
     age=0
     while age <= 0:
@@ -14,12 +14,14 @@ while True:
         sex = str(input("Male or Female? [M/F]: ")).strip().upper()[0]
     if sex == "F" and age < 20:
         females += 1
-    if sex == "M" and age > 18:
+    if sex == "M":
         males+=1
+    if age >=18:
+        maiority+=1
     count+=1
     cont=" "
     while cont not in "YN":
         cont = str(input("Wants to continue? [Y/N] ")).strip().upper()[0]
     if cont != "Y":
         break
-print("{} people / {} men older than 18 / {} girls younger than 20".format(count,males,females))
+print("{} people / {} older than 18 / {} men / {} girls younger than 20".format(count,maiority,males,females))
