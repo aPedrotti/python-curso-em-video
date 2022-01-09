650eeb7fda0ff8059d1fded9e7c843a296dea4ce
Andre Pedrotti <andrehpedrotti@gmail.com>
2022-01-09 11:50:42 -0300

    Answers

--- mundo2/4-loops-with-breaks/challenges/5.py
+++ mundo2/4-loops-with-breaks/challenges/5.py
@@ -3,3 +3,32 @@ print ("""070
 Read product and prices of a purchase process and asks if want to continue.
 Then shows total spent, how much products more expensive than $1000 and what is the name of the cheepest product
 """)
+
+cheepest=count=expensives=total=thousands=0
+cheepest=99999.00
+cheepest_name=" "
+
+print("-="*16)
+print(" "*10,"Happy Mall")
+print("-="*16)
+while True:
+    product = str(input("Enter product Name: ")).strip()
+    price = float(input("Enter product's price: $"))
+    count+=1
+    if price >=1000:
+        expensives+=1
+    if count == 1 or price < cheepest:
+        cheepest_name=product
+        cheepest=price
+    total+=price
+    cont=" "
+    while cont not in "YN":
+        cont = str(input("Would like to continue? [Y/N] ")).strip().upper()[0]
+    print("-"*32)
+    if cont == "N":
+        break
+print("{:^32}".format("CHECKOUT"))
+print("""Total: ${:.2f}
+You bought {} products more expensive than 1000
+The Cheepest product was {}
+""".format(total,expensives,cheepest_name))
--- mundo2/4-loops-with-breaks/challenges/6.py
+++ mundo2/4-loops-with-breaks/challenges/6.py
@@ -2,3 +2,30 @@
 print ("""071
 Simulate and ATM machine asking how much would like to cash and tell how much of each bill will be delivered (50, 20, 10, 5, 1)
 """)
+
+print("="*30)
+print("{:^30}".format("Python Bank"))
+print("="*30)
+cash = int(input("Enter how much would like to cash: "))
+total = cash
+bill=50
+bills=0
+while True:
+    if total >= bill:
+        total-=bill
+        bills+=1
+    else:
+        if bills > 0:
+            print("{} bills of ${}".format(bills,bill))
+        if bill == 50:
+            bill=20
+        elif bill == 20:
+            bill=10
+        elif bill == 10:
+            bill=5
+        elif bill == 5:
+            bill=1
+        bills=0
+        if total == 0:
+            break
+print("-"*5,"Have a good day. Take care","-"*5)
