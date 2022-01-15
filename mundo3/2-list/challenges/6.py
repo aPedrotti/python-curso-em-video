2069fb091bd1ee8c1176a80ed13cf9dc756fda69
Andre Pedrotti <andrehpedrotti@gmail.com>
2022-01-15 20:21:00 -0300

    Answers

--- mundo3/2-list/challenges/6.py
+++ mundo3/2-list/challenges/6.py
@@ -1,5 +1,32 @@
 #!/usr/bin/python
+from cmath import exp
+
+
 print ("""083
 Reads and expression and tells if it is formated correctly with the parentesis
 ex: ((a+b)*c) # Correct
 """)
+
+expression = list(str(input("Enter an expression: ")))
+
+if expression.count("(") == expression.count(")"):
+    print("It is a VALID Expression!")
+else:
+    print("It is INVALID Expression!")
+
+#Answer from class
+expr=str(input("Enter an expression: "))
+pile=[]
+for p in expr:
+    if p == "(":
+        pile.append("(")
+    elif p == ")":
+        if len(pile) > 0:
+            pile.pop()
+        else:
+            pile.append(")")
+            break
+if len(pile) == 0:
+    print('It is Valid Expression')
+else:
+    print('It is Invalid Expression')
