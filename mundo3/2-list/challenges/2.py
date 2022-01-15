2d223d2d17693adcc49fd13127f207c293dbd6ab
pedrotti <andre.oliveira3@gerdau.com.br>
2022-01-15 13:20:07 -0300

    Answer

--- mundo3/2-list/challenges/2.py
+++ mundo3/2-list/challenges/2.py
@@ -3,3 +3,17 @@ print ("""079
 Enter N (wants to continue?) values to a list. If number exists, do not consider.
 Shows numbers in progressive order
 """)
+number=0
+numbers=[]
+while True:
+  number = int(input("=== Enter a number: "))
+  if number not in numbers:
+    numbers.append(number)
+    print("Added successfuly",number)
+  else:
+    print("- Number already exists. Not included")
+  cont = str(input("Want to continue? [Y/N] ")).strip().upper()[0]
+  if cont == "N":
+    break
+#numbers.sort()
+print("Sorted numbers are ",sorted(numbers))
