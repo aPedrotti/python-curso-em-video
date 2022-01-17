acb2799817bf91a90f7accb8be1c7ea232a1beab
Andre Pedrotti <andrehpedrotti@gmail.com>
2022-01-17 20:31:08 -0300

    Answers

--- mundo3/3-dictionaries/challenges/1.py
+++ mundo3/3-dictionaries/challenges/1.py
@@ -7,7 +7,21 @@ Avg for <name>:
 
 Name is <name>
 Avg is <avg>
-Situation is <Failed/Succeded>
+Situation is <Failed/Succeded >=7>
 
 """)
 
+student = {}
+
+student['name'] = str(input("Name: ")).capitalize()
+student['score'] = float(input(f"Average Score of {student['name']}: "))
+if student['score'] >= 7:
+    student['status'] = "Succeded"
+elif 5 <= student['score'] < 7:
+    student['status'] = "Probation"
+else:
+    student['status'] = "Failed"
+
+print(f"Student {student['name']}")
+print(f"Scored {student['score']}")
+print(f"Situation is: {student['status']}")
