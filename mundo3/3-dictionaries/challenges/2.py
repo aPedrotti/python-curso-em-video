c69d683dca0fd37ff5f3ddb6e85a1f03919eb2a2
pedrotti <andre.oliveira3@gerdau.com.br>
2022-01-20 20:39:34 -0300

    Answers

--- mundo3/3-dictionaries/challenges/2.py
+++ mundo3/3-dictionaries/challenges/2.py
@@ -10,3 +10,23 @@ Ranking:
     1o place: player3 with 6
     2o place: player4 with 4...
 """)
+
+from random import randint
+from time import sleep
+from operator import itemgetter
+
+game = {'1': randint(1,6),
+        '2': randint(1,6),
+        '3': randint(1,6),
+        '4': randint(1,6)}
+ranking = {}
+print("-="*10)
+for k,v in game.items():
+    print(f"Player {k} got {v} number")
+    sleep(1)
+
+ranking = sorted(game.items(), key=itemgetter(1), reverse=True) # if 0 ordered by value | Transforms in list
+print("-="*10)
+print("Winners are... ")
+for k,v in ranking:
+    print(f"Player {k} who got {v}")
