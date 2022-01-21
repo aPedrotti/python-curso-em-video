213c6285fccd416439c9334bc58a82fad70e03b4
openshift <jenkins@localhost>
2022-01-21 20:27:39 -0300

    Save progress

--- mundo3/3-dictionaries/challenges/3.py
+++ mundo3/3-dictionaries/challenges/3.py
@@ -3,3 +3,12 @@ print ("""092
 Read name, date of birth and work id. Record age, based on birthday. If work id !=0 will also record the year o hiring and salary.
 Calculate, despite of age, how old he/she will retire
 """)
+
+worker={}
+
+worker['name']
+worker['date_birth']
+worker['labor_id'] = int(input("Enter your Labor ID (0 if you don't have): "))
+if worker['labor_id'] != 0:
+  worker['labor_year'] = int(input("Enter year you've started working: "))
+  worker['labor_salary'] = int(input("Enter your salary: "))
