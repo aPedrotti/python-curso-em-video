4c785a91f3b01139ccbf63c68b6f15e4a4d05424
Andre Pedrotti <andrehpedrotti@gmail.com>
2022-01-23 20:21:29 -0300

    Progress...

--- mundo3/3-dictionaries/challenges/5.py
+++ mundo3/3-dictionaries/challenges/5.py
@@ -7,3 +7,20 @@ Age avg
 list with all women
 list of all people above avg age
 """)
+
+people=[]
+person={}
+
+while True:
+    person['name'] = str(input("Name: "))
+    person['sex'] = str(input("Sex: [M/F] ")).strip().upper()[0]
+    person['age'] = int(input("Age: "))
+    people.append(person[:])
+    cont = str(input("Would like to continue? [Y/N] ")).strip().upper()[0]
+    if cont == "N":
+        break
+print("-="*10)
+for i,v in enumerate(people):
+    for k,v in person.items():
+        print(f"{k} = {v}")
+print(people)
